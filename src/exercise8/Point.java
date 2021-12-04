cc13fb3e3115f12f6ef4c8f6ed6d167925e4dec3
ST3607 <>
2021-12-04 21:49:24 +0000

    Files

--- src/exercise8/Point.java
+++ src/exercise8/Point.java
@@ -1,40 +1,66 @@
 package exercise8;
 
+import java.util.Scanner;
+
 public class Point {
-    // Define attributes
+  double x;
+  double y;
 
-    public Point() {
-        // Initialize attributes to 0
+  public Point() {
+    x = 0;
+    y = 0;
+  }
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public Point(Point otherPoint) {
+    this.x = otherPoint.x;
+    this.y = otherPoint.y;
+  }
+
+  public void initialize() {
+    Scanner input = new Scanner(System.in);
+    x = input.nextDouble();
+    y = input.nextDouble();
+  }
+
+  public void translate(double xDelta, double yDelta) {
+    this.x = x + xDelta;
+    this.y = y + yDelta;
+  }
+
+  public Point createNewTranslatePoint(double xDelta, double yDelta) {
+    Point np = new Point();
+    np.x = x + xDelta;
+    np.y = y + yDelta;
+    return np;
+  }
+
+  public boolean equals(Point otherPoint) {
+    if (this.x == otherPoint.x && this.y == otherPoint.y) {
+      return true;
+    } else {
+      return false;
     }
 
-    public Point(double x, double y) {
-        // Initialize attributes by coping x and y parameters
-    }
+  }
 
-    public Point(Point otherPoint) {
-        // Initialize attributes by coping attributes of otherPoint
-    }
+  public String toString() {
 
-    public void initialize() {
-        // Change attributes through console
-    }
-
-    public void translate(double xDelta, double yDelta) {
-        // Change attributes by adding xDelta and yDelta
-    }
-
-    public Point createNewTranslatedPoint(double xDelta, double yDelta) {
-        // Create new point which is translated relative to this point
-        return null;
-    }
-
-    public boolean equals(Point otherPoint) {
-        // Compare attributes of this point to attributes of otherPoint for equality
-        return false;
-    }
-
-    public String toString() {
-        // Represent attributes as string
-        return null;
-    }
+    return "(" + x + ", " + y + ")";
+  }
 }
+    
+
+  
+  
+        
+    
+
+    
+    
+    
+
